2ada3499cfdfa2863913a90cbdc76a638bd3db25
myemail <myemail@qq.com>
2019-12-10 22:03:46 +0800

    测试使用

--- src/main/java/com/test/Main/Main.java
+++ src/main/java/com/test/Main/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("执行了");
     }
+    public static void two(){
+        System.out.println("two");
+    }
 }
 
